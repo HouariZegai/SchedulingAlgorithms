c543c4d3f3548daa528601dab2fae9a9baaf51ae
HouariZegai <houarizegai14@gmail.com>
2019-12-23 01:26:24 +0100

    Test the DWRR Algorithm [finish with DWRR engine]

---
+++ src/main/java/com/houarizegai/schedulingalgorithms/engine/dwrr/DWRRDemo.java
@@ -0,0 +1,22 @@
+package com.houarizegai.schedulingalgorithms.engine.dwrr;
+
+import com.houarizegai.schedulingalgorithms.engine.dwrr.model.DWRRInputModel;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DWRRDemo {
+    public static void main(String[] args) {
+        List<DWRRInputModel> data = Arrays.asList(
+                new DWRRInputModel(1, "1000", "400", "300"),
+                new DWRRInputModel(2, "600", "300", "200"),
+                new DWRRInputModel(2.5, "500", null, "400"),
+                new DWRRInputModel(3, null, "400", null),
+                new DWRRInputModel(3.5, "600", "300", "500"),
+                new DWRRInputModel(4, "400", "600", "400"),
+                new DWRRInputModel(5, "500", "500", "300"));
+
+        DWRREngine dwrrEngine = new DWRREngine(data, 500, 500, 500);
+        System.out.println(dwrrEngine.getResult());
+    }
+}
